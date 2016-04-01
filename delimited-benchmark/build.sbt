e8d3e965026dde5e95005e71bfde3239b2214ac6
Tom Switzer <thomas.switzer@gmail.com>
2016-04-01 09:55:08 -0400

    Add super basic benchmark for parsing.

---
+++ delimited-benchmark/build.sbt
@@ -0,0 +1,3 @@
+name := "delimited-benchmark"
+
+enablePlugins(JmhPlugin)
---
+++ delimited-benchmark/src/main/scala/net/tixxit/delimited/benchmark/DelimitedParserBenchmark.scala
@@ -0,0 +1,35 @@
+package net.tixxit.delimited
+package benchmark
+
+import org.openjdk.jmh.annotations.{Benchmark, Scope, State}
+
+import net.tixxit.delimited.parser._
+
+class DelimitedParserBenchmark {
+  import DelimitedParserBenchmark._
+
+  @Benchmark
+  def parseNarrowTSV(data: Data): Vector[Either[DelimitedError, Row]] = {
+    DelimitedParser(data.format).parseString(data.narrowTsv)
+  }
+
+  @Benchmark
+  def parseWideTSV(data: Data): Vector[Either[DelimitedError, Row]] = {
+    DelimitedParser(data.format).parseString(data.wideTsv)
+  }
+}
+
+object DelimitedParserBenchmark {
+
+  @State(Scope.Benchmark)
+  class Data {
+    val format: DelimitedFormat = DelimitedFormat.TSV
+    val narrowRow: Row = Row("aaaaaa", "aa", "aaaaaaaaaaaaaaaaaaaaaaaaa")
+    val narrowTsv: String =
+      List.fill(1000)(narrowRow.render(format)).mkString(format.rowDelim.value)
+
+    val wideRow: Row = Row(List.fill(30)(narrowRow).flatten: _*)
+    val wideTsv: String =
+      List.fill(1000)(wideRow.render(format)).mkString(format.rowDelim.value)
+  }
+}
