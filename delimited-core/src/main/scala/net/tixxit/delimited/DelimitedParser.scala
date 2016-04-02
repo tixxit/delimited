298860bfc08725fda60bc80701e47a17c4cfe3fc
Tom Switzer <thomas.switzer@gmail.com>
2016-04-01 23:13:58 -0400

    Document DelimitedParser.

--- delimited-core/src/main/scala/net/tixxit/delimited/DelimitedParser.scala
+++ delimited-core/src/main/scala/net/tixxit/delimited/DelimitedParser.scala
@@ -5,11 +5,44 @@ import java.io.File
 import java.io.{ InputStream, FileInputStream }
 import java.io.{ Reader, InputStreamReader }
 
+/**
+ * An immutable parser for delimited files. This operates on chunks of input,
+ * using the `parseChunk` method. After parsing a chunk, the `parseChunk` method
+ * returns a new `DelimitedParser` as well as all of the complete rows parsed
+ * in that chunk. Any partially complete rows will be returned in a future call
+ * to `parseChunk` in either the returned `DelimitedParser` or a future one in
+ * a chain of calls to `parseChunk`.
+ *
+ * There are also convenience methods for parsing `File`s, `String`s,
+ * `InputStream`s, `Reader`s, etc.
+ */
 trait DelimitedParser {
+
+  /**
+   * The [[DelimitedFormat]] being used to parse this delimited file, or `None`
+   * if a format has not yet been inferred (in which case, no rows have yet
+   * been returned by `parseChunk`).
+   */
   def format: Option[DelimitedFormat]
 
+  /**
+   * Parse a chunk of the input if there is any left. If chunk is `None`, then
+   * that indicates to the parser that there will be no further input. In this
+   * case (`chunk` is `None`), all remaining input will be consumed and
+   * returned as rows (or errors).
+   *
+   * This returns a new `DelimitedParser` to use to parse the next chunk, as
+   * well as a `Vector` of all complete rows parsed from `chunk`.
+   *
+   * @param chunk the next chunk of data as a String, or None if eof
+   */
   def parseChunk(chunk: Option[String]): (DelimitedParser, Vector[Either[DelimitedError, Row]])
 
+  /**
+   * Parse all chunks in the given iterator, consecutively, treating the last
+   * chunk in `chunks` as the final input. This will return all rows from the
+   * input.
+   */
   def parseAll(chunks: Iterator[String]): Iterator[Either[DelimitedError, Row]] = {
     val input = chunks.map(Option(_)).takeWhile(_.isDefined) ++ Iterator(None)
 
@@ -20,6 +53,10 @@ trait DelimitedParser {
       .flatMap(_._2)
   }
 
+  /**
+   * Returns an iterator that parses rows from `reader` as elements are
+   * consumed.
+   */
   def parseReader(reader: Reader): Iterator[Either[DelimitedError, Row]] = {
     val buffer = new Array[Char](DelimitedParser.BufferSize)
     val chunks = Iterator.continually {
@@ -34,9 +71,22 @@ trait DelimitedParser {
     parseAll(chunks)
   }
 
+  /**
+   * Returns an iterator that parses rows from `in` as elements are
+   * consumed.
+   *
+   * @param in      the raw input as bytes
+   * @param charset the character set to decode the bytes as
+   */
   def parseInputStream(is: InputStream, charset: Charset = StandardCharsets.UTF_8): Iterator[Either[DelimitedError, Row]] =
     parseReader(new InputStreamReader(is, charset))
 
+  /**
+   * Completely parses `file` and returns all the rows in a `Vector`.
+   *
+   * @param file    the TSV file on disk
+   * @param charset the character set the TSV was encoded in
+   */
   def parseFile(file: File, charset: Charset = StandardCharsets.UTF_8): Vector[Either[DelimitedError, Row]] = {
     val is = new FileInputStream(file)
     try {
@@ -46,6 +96,9 @@ trait DelimitedParser {
     }
   }
 
+  /**
+   * Parses an entire delimited file as a string.
+   */
   def parseString(input: String): Vector[Either[DelimitedError, Row]] =
     parseAll(Iterator(input)).toVector
 }
@@ -53,8 +106,16 @@ trait DelimitedParser {
 object DelimitedParser {
   val BufferSize = 64 * 1024
 
+  /**
+   * Returns a `DelimitedParser` that will infer all parameters in its
+   * [[DelimitedFormat]].
+   */
   def apply(): DelimitedParser = apply(DelimitedFormat.Guess)
 
+  /**
+   * Returns a `DelimitedParser` that can parse delimited files using the
+   * strategy or format provided.
+   */
   def apply(format: DelimitedFormatStrategy): DelimitedParser =
     parser.DelimitedParserImpl(format)
 }
