9483bc2244cca25daf32f3b8a7f629ee690f8614
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-07-24 01:15:02 +0200

    Update scalatest to 3.1.2 (#42)
    Co-authored-by: P. Oscar Boykin <johnynek@users.noreply.github.com>

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt._
 object Deps {
   object V {
     val iteratee   = "0.19.0"
-    val scalaTest = "3.1.1"
+    val scalaTest = "3.1.2"
     val scalaTestPlusCheck  = "3.1.2.0"
     val scalaCheck = "1.14.3"
   }
