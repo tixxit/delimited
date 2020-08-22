2bb63490c3e342f1f6779a3d4f8f4847c4a3258b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-22 18:51:05 +0200

    Update scalacheck-1-14 to 3.2.2.0 (#59)

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,7 +4,7 @@ object Deps {
   object V {
     val iteratee   = "0.19.0"
     val scalaTest = "3.1.3"
-    val scalaTestPlusCheck  = "3.2.1.0"
+    val scalaTestPlusCheck  = "3.2.2.0"
     val scalaCheck = "1.14.3"
   }
 
